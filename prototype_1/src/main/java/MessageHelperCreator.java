f94f81842574f21b0ae3407d279bf13ce8452a25
tunashred <enachealexandru.traian@gmail.com>
2025-02-04 14:04:50 +0200

    Fix serializer by transitioning to a MessageInfo list

--- prototype_1/src/main/java/MessageHelperCreator.java
+++ prototype_1/src/main/java/MessageHelperCreator.java
@@ -1,36 +1,33 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class MessageHelperCreator {
     public static void main(String[] args) throws IOException {
         File file = new File("input_messages/mixed_messages.json");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         ObjectMapper objectMapper = new ObjectMapper();
 
         GroupChat groupChat = new GroupChat("chat_test", "01236969");
         User user = new User("Ionel", "189432");
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
-
-        String message;
+        List<MessageInfo> messages = new ArrayList<>();
         while (true) {
-            message = reader.readLine();
+            String message = reader.readLine();
             if (Objects.equals(message, "0")) {
                 break;
             }
             MessageInfo messageInfo = new MessageInfo(groupChat, user, message);
-            String serialized = objectMapper.writeValueAsString(messageInfo);
+            messages.add(messageInfo);
 
-            writer.write(serialized);
-            writer.newLine();
             System.out.println("Group chat: " + groupChat.getChatName() + "/" + groupChat.getChatID() +
                     "\nUser: " + user.getName() + "/" + user.getUserID() +
                     "\nMessage: " + message);
 
         }
-        writer.flush();
-        writer.close();
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, messages);
     }
 }
