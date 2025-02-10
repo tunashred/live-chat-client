eea0d21b9e326b05a6a5d3aac44e0a0fc0512b5e
tunashred <enachealexandru.traian@gmail.com>
2025-02-10 19:10:56 +0200

    Split consumer and producer into separate classes which implement Runnable interface

---
+++ live-chat-public-client/src/main/java/com/github/tunashred/clients/ConsumerRunnable.java
@@ -0,0 +1,57 @@
+package com.github.tunashred.clients;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tunashred.dtos.MessageInfo;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+
+public class ConsumerRunnable implements Runnable {
+    private AtomicBoolean keepRunnning = new AtomicBoolean(true);
+
+    public void stopRunning() {
+        keepRunnning.set(false);
+    }
+
+    @Override
+    public void run() {
+        Properties consumerProps = new Properties();
+        consumerProps.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        consumerProps.put(GROUP_ID_CONFIG, "moderator-consumer-group");
+        consumerProps.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProps.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProps.put(ENABLE_AUTO_COMMIT_CONFIG, "false");
+        consumerProps.put(AUTO_OFFSET_RESET_CONFIG, "latest");
+        consumerProps.put("acks", "all");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        while (keepRunnning.get()) {
+            try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProps)) {
+                consumer.subscribe(Collections.singletonList("safe_chat"));
+
+                while (keepRunnning.get()) {
+                    ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
+                    for (var record : consumerRecords) {
+                        try {
+                            MessageInfo messageInfo = objectMapper.readValue(record.value(), MessageInfo.class);
+                            System.out.println("\nGroup chat: " + messageInfo.getGroupChat().getChatName() + "/" + messageInfo.getGroupChat().getChatID() +
+                                    "\nmessage.User: " + messageInfo.getUser().getName() + "/" + messageInfo.getUser().getUserID() +
+                                    "\nOriginal message: " + messageInfo.getMessage());
+                        } catch (JsonProcessingException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ live-chat-public-client/src/main/java/com/github/tunashred/clients/ProducerRunnable.java
@@ -0,0 +1,51 @@
+package com.github.tunashred.clients;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tunashred.dtos.GroupChat;
+import com.github.tunashred.dtos.MessageInfo;
+import com.github.tunashred.dtos.User;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+public class ProducerRunnable implements Runnable {
+    private AtomicBoolean keepRunnning = new AtomicBoolean(true);
+
+    public void stopRunning() {
+        keepRunnning.set(false);
+    }
+
+    @Override
+    public void run() {
+        Properties producerProps = new Properties();
+        producerProps.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        producerProps.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProps.put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ObjectMapper objectMapper = new ObjectMapper();
+        while (keepRunnning.get()) {
+            try (KafkaProducer<String, String> producer = new KafkaProducer<>(producerProps)) {
+                while (keepRunnning.get()) {
+                    try {
+                        MessageInfo messageInfo = new MessageInfo(new GroupChat("some group", "6969"), new User("some user", "13018"), reader.readLine());
+                        String serialized = objectMapper.writeValueAsString(messageInfo);
+
+                        ProducerRecord<String, String> record = new ProducerRecord<>("unsafe_chat", messageInfo.getGroupChat().getChatID(), serialized);
+                        producer.send(record);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+}
