9a0bd74ca5994d1f94a960dd4cd7f0d12c070c78
tunashred <enachealexandru.traian@gmail.com>
2025-02-20 20:01:16 +0200

    Add a way for more 'real' values from the user part

--- live-chat-client/src/main/java/com/github/tunashred/clients/ProducerRunnable.java
+++ live-chat-client/src/main/java/com/github/tunashred/clients/ProducerRunnable.java
@@ -3,6 +3,7 @@ package com.github.tunashred.clients;
 import com.github.tunashred.dtos.GroupChat;
 import com.github.tunashred.dtos.MessageInfo;
 import com.github.tunashred.dtos.User;
+import com.github.tunashred.utils.GroupchatCreatorReader;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -10,6 +11,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -31,10 +33,29 @@ public class ProducerRunnable implements Runnable {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (keepRunnning.get()) {
+            // try initializing producer and choosing a group and name for the user
             try (KafkaProducer<String, String> producer = new KafkaProducer<>(producerProps)) {
+                List<GroupChat> groups = GroupchatCreatorReader.getGroups("input/groups.txt");
+                GroupchatCreatorReader.printGroups(groups);
+                System.out.println("Choose a group to join: ");
+
+                String input = reader.readLine();
+                int selection = Integer.parseInt(input);
+                while (selection < 0 || selection > groups.size() - 1) {
+                    System.out.println("Invalid selection. Please enter a number between " + 0 + " and " + (groups.size() - 1));
+                    input = reader.readLine();
+                    selection = Integer.parseInt(input);
+                }
+                // TODO: ctor with object arg
+                GroupChat chosenGroup = new GroupChat(groups.get(selection).getChatName(), groups.get(selection).getChatID());
+
+                System.out.println("Please choose your username: ");
+                input = reader.readLine();
+                // TODO: maybe move hashing part to a separate function
+                User user = new User(input, String.valueOf(input.length()));
                 while (keepRunnning.get()) {
                     try {
-                        MessageInfo messageInfo = new MessageInfo(new GroupChat("some group", "6969"), new User("some user", "13018"), reader.readLine());
+                        MessageInfo messageInfo = new MessageInfo(chosenGroup, user, reader.readLine());
                         String serialized = MessageInfo.serialize(messageInfo);
 
                         ProducerRecord<String, String> record = new ProducerRecord<>("unsafe_chat", messageInfo.getGroupChat().getChatID(), serialized);
@@ -43,6 +64,8 @@ public class ProducerRunnable implements Runnable {
                         e.printStackTrace();
                     }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
--- live-chat-client/src/main/java/com/github/tunashred/utils/GroupchatCreatorReader.java
+++ live-chat-client/src/main/java/com/github/tunashred/utils/GroupchatCreatorReader.java
@@ -12,7 +12,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GroupchatCreator {
+public class GroupchatCreatorReader {
     public static void main(String[] args) throws IOException {
         File file = new File("input/groups.txt");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -36,5 +36,28 @@ public class GroupchatCreator {
         System.out.println("Group name: " + groupName + "\nGroup ID: " + group.getChatID());
 
         mapper.writerWithDefaultPrettyPrinter().writeValue(file, groups);
+    }
+
+    public static List<GroupChat> getGroups(String filePath) throws IOException {
+        File file = new File(filePath);
+        ObjectMapper mapper = new ObjectMapper();
+
+        if (!file.exists() || file.length() == 0) {
+            System.out.println("File empty or does not exist.");
+            return null;
+        }
+
+        return mapper.readValue(file, new TypeReference<List<GroupChat>>() {
+        });
+    }
+
+    public static void printGroups(List<GroupChat> groups) {
+        int i = 0;
+        for (var group : groups) {
+            System.out.println(i + "." + " Group Name: " + group.getChatName());
+            System.out.println("Group ID: " + group.getChatID());
+            System.out.println("-------------------------");
+            i++;
+        }
     }
 }
