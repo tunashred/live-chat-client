f95c963f549792569cc938c169bcb7723be3eb84
tunashred <enachealexandru.traian@gmail.com>
2025-03-11 21:15:40 +0200

    Add ConsumerRunnable and ProducerRunnable fields for possible future need

--- live-chat-client/src/main/java/com/github/tunashred/clients/ChatSession.java
+++ live-chat-client/src/main/java/com/github/tunashred/clients/ChatSession.java
@@ -6,17 +6,23 @@ import com.github.tunashred.dtos.User;
 public class ChatSession {
     User user;
     GroupChat groupChat;
-    Thread consumer;
-    Thread producer;
+
+    ConsumerRunnable consumer;
+    ProducerRunnable producer;
+
+    Thread consumerThread;
+    Thread producerThread;
 
     public ChatSession(User user, GroupChat groupChat) {
         this.user = user;
         this.groupChat = groupChat;
 
-        this.consumer = new Thread(new ConsumerRunnable(groupChat.getChatName()));
-        consumer.start();
+        this.consumer = new ConsumerRunnable(groupChat.getChatName());
+        this.consumerThread = new Thread(this.consumer);
+        consumerThread.start();
 
-        this.producer = new Thread(new ProducerRunnable(user, groupChat));
-        producer.start();
+        this.producer = new ProducerRunnable(user, groupChat);
+        this.producerThread = new Thread(this.producer);
+        producerThread.start();
     }
 }
